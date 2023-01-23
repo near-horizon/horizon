249c6eb9f98c4fcb141a7649c379274049b8c74b
Petar Vujović <petarvujovic98@gmail.com>
2023-01-23 16:42:09 +0100

    fix: Create res folder it it doesn't exist

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
+[[ ! -d "res" ]] && mkdir res
 cp target/wasm32-unknown-unknown/release/near_contribute.wasm ./res/
 
