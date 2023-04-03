bc2d63e7c1d6f779393344abf413dd6c397a5ee0
Petar Vujović <petarvujovic98@gmail.com>
2023-04-03 13:58:05 +0200

    fix: Add pagination for large key number

--- dirty_deploy.sh
+++ dirty_deploy.sh
@@ -4,10 +4,19 @@ set -e
 ACCOUNT_ID=$1
 export NEAR_ENV=mainnet
 
-KEYS=$(near view-state $ACCOUNT_ID --finality final | sed -e "s/'/\"/g" | sed -e "s/key/\"key\"/g" | sed -e "s/value/\"value\"/g" | jq '[.[] | .key]' | tr -d '\n' | tr -d '[:blank:]')
+KEYS=$(near view-state $ACCOUNT_ID --finality final | sed -e "s/'/\"/g" | sed -e "s/key/\"key\"/g" | sed -e "s/value/\"value\"/g" | jq '[.[] | .key]')
+KEYS_LENGTH=$(echo $KEYS | jq 'length');
+
+KEY_LIMIT=100
 
 near deploy $ACCOUNT_ID ./state_cleanup.wasm
-near call $ACCOUNT_ID clean "{\"keys\":$KEYS}" --accountId $ACCOUNT_ID
+
+for ((index = 0; index < $KEYS_LENGTH; index = index + $KEY_LIMIT))
+do
+  export SLICE=$(echo $KEYS | jq ".[$index:$((index + KEY_LIMIT))]" | tr -d '\n' | tr -d '[:blank:]')
+  near call $ACCOUNT_ID clean "{\"keys\":$SLICE}" --accountId $ACCOUNT_ID
+  unset SLICE
+done
 
 near deploy $ACCOUNT_ID ./res/near_contribute.wasm
 
