2b1d99d482c2655df7b3cccf64de539d3d671827
Leandro Manzanal <leomanzanal@gmail.com>
2023-04-15 06:42:07 -0300

    add project request list widget

---
+++ widgets/src/Project/Requests.jsx
@@ -0,0 +1,32 @@
+const ownerId = "contribut3.near";
+const account_id = props.accountId ?? "contribute3.near"
+const search = props.search ?? "";
+
+State.init({
+    items: [],
+    itemsIsFetched: false,
+});
+
+if (!state.itemsIsFetched) {
+    Near.asyncView(ownerId, "get_project_requests", { account_id }, "final", false).then((items) =>
+        State.update({ items, itemsIsFetched: true })
+    );
+
+    return <>Loading...</>;
+}
+
+return (
+    <Widget
+        src={`${ownerId}/widget/List`}
+        props={{
+            search,
+            items: state.items,
+            createItem: ([accountId, cid]) => (
+                <Widget
+                    src={`${ownerId}/widget/Request.Card`}
+                    props={{ accountId, cid }}
+                />
+            ),
+        }}
+    />
+);
