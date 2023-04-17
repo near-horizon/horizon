5a0c2c9eb81e5a0f159ad8a06d7f4a366c368348
Petar Vujović <petarvujovic98@gmail.com>
2023-04-17 08:48:46 +0200

    chore: Format files

--- widgets/src/Project/Requests.jsx
+++ widgets/src/Project/Requests.jsx
@@ -3,30 +3,34 @@ const account_id = props.accountId;
 const search = props.search ?? "";
 
 State.init({
-    items: [],
-    itemsIsFetched: false,
+  items: [],
+  itemsIsFetched: false,
 });
 
 if (!state.itemsIsFetched) {
-    Near.asyncView(ownerId, "get_project_requests", { account_id }, "final", false).then((items) =>
-        State.update({ items, itemsIsFetched: true })
-    );
+  Near.asyncView(
+    ownerId,
+    "get_project_requests",
+    { account_id },
+    "final",
+    false
+  ).then((items) => State.update({ items, itemsIsFetched: true }));
 
-    return <>Loading...</>;
+  return <>Loading...</>;
 }
 
 return (
-    <Widget
-        src={`${ownerId}/widget/List`}
-        props={{
-            search,
-            items: state.items,
-            createItem: ([accountId, cid]) => (
-                <Widget
-                    src={`${ownerId}/widget/Request.Card`}
-                    props={{ accountId, cid }}
-                />
-            ),
-        }}
-    />
+  <Widget
+    src={`${ownerId}/widget/List`}
+    props={{
+      search,
+      items: state.items,
+      createItem: ([accountId, cid]) => (
+        <Widget
+          src={`${ownerId}/widget/Request.Card`}
+          props={{ accountId, cid }}
+        />
+      ),
+    }}
+  />
 );
