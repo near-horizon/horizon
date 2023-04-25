b6b3dd81b79ced171cbe63f7408914fb7f2e4922
Petar Vujović <petarvujovic98@gmail.com>
2023-04-25 17:20:36 +0200

    feat: Fetch data from nearatlas

--- widgets/src/Dashboard.jsx
+++ widgets/src/Dashboard.jsx
@@ -126,6 +126,19 @@ const Stats = styled.div`
   }
 `;
 
+State.init({
+  stats: null,
+  statsIsFetched: false,
+});
+
+if (!state.statsIsFetched) {
+  asyncFetch(
+    "https://api.flipsidecrypto.com/api/v2/queries/36637c73-6301-418b-ae83-7af6e8f34c0f/data/latest"
+  ).then((response) =>
+    State.update({ stats: response.body[0], statsIsFetched: true })
+  );
+}
+
 return (
   <Container>
     <Heading>
@@ -143,14 +156,22 @@ return (
       <Widget
         src={`${ownerId}/widget/Stats.Card`}
         props={{
-          value: "1M+",
+          value: state.statsIsFetched
+            ? Number(state.stats.MAU).toLocaleString("en-US", {
+                notation: "compact",
+              }) + "+"
+            : "Loading...",
           label: "Monthly active accounts",
         }}
       />
       <Widget
         src={`${ownerId}/widget/Stats.Card`}
         props={{
-          value: "25M+",
+          value: state.statsIsFetched
+            ? Number(state.stats.TOTAL_ACCOUNTS).toLocaleString("en-US", {
+                notation: "compact",
+              }) + "+"
+            : "Loading...",
           label: "Total accounts",
         }}
       />
