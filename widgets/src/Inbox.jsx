e7abd1d9fe59fca214c217590bad7b970891c2ab
Leandro Manzanal <leomanzanal@gmail.com>
2023-04-25 02:38:45 -0300

    WIP:Inbox notifiaction+Notification manager+Invite

--- widgets/src/Inbox.jsx
+++ widgets/src/Inbox.jsx
@@ -1,5 +1,19 @@
 const ownerId = "contribut3.near";
 
+const Wrapper = styled.div`
+  padding-bottom: 48px;
+
+  > div {
+    > * {
+      margin-bottom: 12px;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+`;
+
 const Header = styled.h1`
   font-style: normal;
   font-weight: 700;
@@ -9,4 +23,32 @@ const Header = styled.h1`
 
 const header = <Header>Inbox</Header>;
 
-return <div>{header}</div>;
+
+const index = {
+  action: "inbox",
+  key: accountId,
+  options: {
+    limit: 10,
+    order: "desc",
+    subscribe: true,
+  },
+};
+
+const renderItem = (item, i) => {
+  if (i === 0) {
+    Storage.set("lastBlockHeight", item.blockHeight);
+  }
+  return (
+    <Widget src={`${ownerId}/widget/Notification`} key={i} props={item} />
+  );
+};
+
+return (
+  <Wrapper>
+    <Header>{header}</Header>
+    <Widget
+      src="near/widget/IndexFeed"
+      props={{ index, renderItem }}
+    />
+  </Wrapper>
+);
