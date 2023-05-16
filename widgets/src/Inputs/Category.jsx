c6b621ad205ee78ae5512084715431f117dbe224
Petar Vujović <petarvujovic98@gmail.com>
2023-05-16 10:53:33 +0200

    feat: Change category options

--- widgets/src/Inputs/Category.jsx
+++ widgets/src/Inputs/Category.jsx
@@ -1,17 +1,50 @@
 const ownerId = "nearhorizon.near";
 
+/** @type {{text:string;value:string}[]} */
 const options = [
-  { text: "Wallets", value: "wallets" },
-  { text: "Gaming/Metaverse", value: "gaming/metaverse" },
   { text: "DeSci", value: "desci" },
+  { text: "DeFi", value: "defi" },
+  { text: "Gaming", value: "gaming" },
+  { text: "Metaverse", value: "metaverse" },
+  { text: "Commercial", value: "commercial" },
+  { text: "Sports and Entertainment", value: "sports-and-entertainment" },
   { text: "Infrastructure", value: "infrastructure" },
-  { text: "NFT", value: "nft" },
-  { text: "DAO", value: "dao" },
-  { text: "Social impact", value: "social-impact" },
-  { text: "Web2 expansion", value: "web2" },
-  { text: "Web3 Product Partner", value: "web3-product-partner" },
+  { text: "Social", value: "social" },
+  { text: "Social Impact", value: "social-impact" },
+  { text: "Creative", value: "creative" },
+  { text: "Education", value: "education" },
   { text: "Other", value: "other" },
 ];
+
+/**
+ * @param {string} old
+ *
+ * @returns {string[]}
+ * */
+const mapOldToNew = (old) => {
+  switch (old) {
+    case "wallets":
+      return ["infrastructure"];
+    case "gaming/metaverse":
+      return ["gaming", "metaverse"];
+    case "nft":
+      return ["creative"];
+    case "dao":
+      return ["social-impact"];
+    default:
+      return [old];
+  }
+};
+
+/**
+ * @param {string} value
+ *
+ * @returns {{text:string;value:string}}
+ * */
+const mapValueToOption = (value) => {
+  const option = options.find((option) => option.value === value);
+  return option ?? { text: value, value };
+};
 
 return (
   <Widget
