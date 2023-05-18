807b0865a0f260ddbd4618f82448150cca22b359
Petar Vujović <petarvujovic98@gmail.com>
2023-05-18 22:01:54 +0200

    fix: Escape double quotes inside string

--- scripts/widget_delete.sh
+++ scripts/widget_delete.sh
@@ -5,7 +5,7 @@ ACCOUNT=$1
 WIDGET_NAME=$2
 export NEAR_ENV=mainnet
 
-near call social.near set "{\"data\":{\"$ACCOUNT\":{\"widget\":{\"$WIDGET_NAME\":{"":null}}}}}" --accountId $ACCOUNT
+near call social.near set "{\"data\":{\"$ACCOUNT\":{\"widget\":{\"$WIDGET_NAME\":{\"\":null}}}}}" --accountId $ACCOUNT
 near call social.near set "{\"data\":{\"$ACCOUNT\":{\"widget\":{\"$WIDGET_NAME\":null}}}}" --accountId $ACCOUNT
 
 unset NEAR_ENV
