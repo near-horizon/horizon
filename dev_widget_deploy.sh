ac7c8c62e45cce2d21adcc0eacea2dad3cba390d
Petar Vujović <petarvujovic98@gmail.com>
2023-06-02 09:10:34 +0200

    feat: Add API URL substitution

--- dev_widget_deploy.sh
+++ dev_widget_deploy.sh
@@ -3,8 +3,11 @@ set -e
 
 OLD_ACCOUNT=$1
 NEW_ACCOUNT=$2
+OLD_API_URL=$3
+NEW_API_URL=$4
 
 fd -e jsx -e json -x sed -i "s/$OLD_ACCOUNT/$NEW_ACCOUNT/g" {}
+fd -e jsx -e json -x sed -i "s,$OLD_API_URL,$NEW_API_URL,g" {}
 
 cd widgets/
 
@@ -13,4 +16,4 @@ npm run deploy
 cd ..
 
 fd -e jsx -e json -x sed -i "s/$NEW_ACCOUNT/$OLD_ACCOUNT/g" {}
-
+fd -e jsx -e json -x sed -i "s,$NEW_API_URL,$OLD_API_URL,g" {}
