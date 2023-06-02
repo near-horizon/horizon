535bd550024d6f2f581270845ad4ae0834f81e79
Petar Vujović <petarvujovic98@gmail.com>
2023-06-02 10:32:55 +0200

    feat: Add initial vendor search and sorting

--- api/src/routes/data/mod.rs
+++ api/src/routes/data/mod.rs
@@ -3,7 +3,10 @@ use axum::Router;
 use crate::AppState;
 
 pub mod projects;
+pub mod vendors;
 
 pub fn create_router() -> Router<AppState> {
-    Router::new().nest("/projects", projects::create_router())
+    Router::new()
+        .nest("/projects", projects::create_router())
+        .nest("/vendors", vendors::create_router())
 }
---
+++ api/src/routes/data/vendors.rs
@@ -0,0 +1,162 @@
+use std::collections::HashSet;
+
+use axum::{
+    debug_handler,
+    extract::{Query, State},
+    routing::get,
+    Json, Router,
+};
+use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+use sqlx::Row;
+
+use crate::AppState;
+
+#[derive(Deserialize, Serialize, Debug, Clone, Copy, Default)]
+#[serde(rename_all = "lowercase")]
+pub enum Sort {
+    TimeAsc,
+    #[default]
+    TimeDesc,
+    NameAsc,
+    NameDesc,
+    RecentAsc,
+    RecentDesc,
+}
+
+impl Sort {
+    pub fn get_order_by(&self) -> (&'static str, &'static str) {
+        match self {
+            Sort::TimeAsc => (
+                r#"
+                LEFT JOIN (
+                    SELECT DISTINCT ON (transactions.args->>'account_id') transactions.args->>'account_id' as account_id, transactions.timestamp
+                    FROM transactions
+                    WHERE transactions.method_name = 'register_vendor' OR transactions.method_name = 'add_vendor'
+                    ORDER BY transactions.args->>'account_id' ASC, transactions.timestamp ASC
+                ) as txs ON vendors.id = txs.account_id
+                "#,
+                "ORDER BY txs.timestamp ASC",
+            ),
+            Sort::TimeDesc => (
+                r#"
+                LEFT JOIN (
+                    SELECT DISTINCT ON (transactions.args->>'account_id') transactions.args->>'account_id' as account_id, transactions.timestamp
+                    FROM transactions
+                    WHERE transactions.method_name = 'register_vendor' OR transactions.method_name = 'add_vendor'
+                    ORDER BY transactions.args->>'account_id' ASC, transactions.timestamp DESC
+                ) as txs ON vendors.id = txs.account_id
+                "#,
+                "ORDER BY txs.timestamp DESC",
+            ),
+            Sort::NameAsc => ("", "ORDER BY vendors.name ASC"),
+            Sort::NameDesc => ("", "ORDER BY vendors.name DESC"),
+            Sort::RecentAsc => (
+                r#"
+                LEFT JOIN (
+                    SELECT DISTINCT ON (COALESCE(transactions.args->>'account_id', transactions.args->>'vendor_id', transactions.args->'proposal'->>'vendor_id'))
+	                    COALESCE(transactions.args->>'account_id', transactions.args->>'vendor_id', transactions.args->'proposal'->>'vendor_id') as account_id, transactions.method_name, transactions.timestamp
+                    FROM transactions
+                    WHERE
+	                    COALESCE(transactions.args->>'account_id', transactions.args->>'vendor_id', transactions.args->'proposal'->>'vendor_id') IS NOT NULL
+	                    AND transactions.method_name IN ('add_vendor', 'edit_vendor', 'register_vendor', 'add_proposal', 'accept_contribution')
+                    ORDER BY COALESCE(transactions.args->>'account_id', transactions.args->>'vendor_id', transactions.args->'proposal'->>'vendor_id') ASC, transactions.timestamp ASC
+                ) as txs ON vendors.id = txs.account_id
+                "#,
+                "ORDER BY txs.timestamp ASC",
+            ),
+            Sort::RecentDesc => (
+                r#"
+                LEFT JOIN (
+                    SELECT DISTINCT ON (COALESCE(transactions.args->>'account_id', transactions.args->>'vendor_id', transactions.args->'proposal'->>'vendor_id'))
+	                    COALESCE(transactions.args->>'account_id', transactions.args->>'vendor_id', transactions.args->'proposal'->>'vendor_id') as account_id, transactions.method_name, transactions.timestamp
+                    FROM transactions
+                    WHERE
+	                    COALESCE(transactions.args->>'account_id', transactions.args->>'vendor_id', transactions.args->'proposal'->>'vendor_id') IS NOT NULL
+	                    AND transactions.method_name IN ('add_vendor', 'edit_vendor', 'register_vendor', 'add_proposal', 'accept_contribution')
+                    ORDER BY COALESCE(transactions.args->>'account_id', transactions.args->>'vendor_id', transactions.args->'proposal'->>'vendor_id') ASC, transactions.timestamp DESC
+                ) as txs ON vendors.id = txs.account_id
+                "#,
+                "ORDER BY txs.timestamp DESC",
+            ),
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone, Default)]
+pub struct Params {
+    #[serde(default)]
+    pub sort: Sort,
+    pub from: Option<u32>,
+    pub limit: Option<u32>,
+    #[serde(rename = "q")]
+    pub search: Option<String>,
+}
+
+#[debug_handler(state = AppState)]
+pub async fn all_vendors(
+    Query(params): Query<Params>,
+    State(AppState { pool, .. }): State<AppState>,
+) -> Result<Json<Vec<String>>, (StatusCode, String)> {
+    let mut builder = sqlx::QueryBuilder::new(
+        r#"
+        SELECT vendors.id
+        FROM vendors
+        "#,
+    );
+
+    let (join, order_by) = params.sort.get_order_by();
+
+    builder.push(join);
+
+    if let Some(search) = params.search {
+        let search = format!("%{}%", search);
+        builder.push(" WHERE vendors.name ILIKE ");
+        builder.push_bind(search.clone());
+        builder.push(" OR vendors.id ILIKE ");
+        builder.push_bind(search.clone());
+        builder.push(" OR vendors.tagline ILIKE ");
+        builder.push_bind(search.clone());
+        builder.push(" OR vendors.description ILIKE ");
+        builder.push_bind(search.clone());
+        builder.push(" OR vendors.services ILIKE ");
+        builder.push_bind(search);
+    }
+
+    builder.push(format!(" {order_by}"));
+
+    if let Some(limit) = params.limit {
+        builder.push(" LIMIT ");
+        builder.push_bind(limit as i32);
+    }
+
+    if let Some(from) = params.from {
+        builder.push(" OFFSET ");
+        builder.push_bind(from as i32);
+    }
+
+    let result = builder.build().fetch_all(&pool).await.map_err(|e| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Failed to get vendors: {}", e),
+        )
+    })?;
+
+    Ok(Json({
+        let mut seen = HashSet::<String>::new();
+        result
+            .into_iter()
+            .filter_map(|r| {
+                let id: String = r.get("id");
+                (!seen.contains(&id)).then_some({
+                    seen.insert(id.clone());
+                    id
+                })
+            })
+            .collect()
+    }))
+}
+
+pub fn create_router() -> Router<AppState> {
+    Router::new().route("/", get(all_vendors))
+}
