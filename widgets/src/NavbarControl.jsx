5f3013f51f6a6d0360a8e47075a21ec88650e641
Petar Vujović <petarvujovic98@gmail.com>
2023-06-06 13:03:29 +0200

    chore: Remove credits from navbar

--- widgets/src/NavbarControl.jsx
+++ widgets/src/NavbarControl.jsx
@@ -1,9 +1,5 @@
 const ownerId = "nearhorizon.near";
 
-State.init({
-  credits: 0,
-});
-
 const Navbar = styled.div`
   z-index: 2;
   display: flex;
@@ -89,31 +85,8 @@ const Stats = styled.div`
   line-height: 1em;
 `;
 
-const stats = (
-  <Stats>
-    <span>Available credits:</span>
-    <Credits>{state.credits} NHZN</Credits>
-    <svg
-      width="14"
-      height="14"
-      viewBox="0 0 14 14"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M5.30283 5.2513C5.43997 4.86144 5.71066 4.5327 6.06697 4.3233C6.42327 4.11389 6.84218 4.03735 7.24951 4.10722C7.65684 4.17709 8.0263 4.38886 8.29245 4.70503C8.55861 5.0212 8.70427 5.42136 8.70366 5.83464C8.70366 7.0013 6.95366 7.58464 6.95366 7.58464M7.00033 9.91797H7.00616M12.8337 7.0013C12.8337 10.223 10.222 12.8346 7.00033 12.8346C3.77866 12.8346 1.16699 10.223 1.16699 7.0013C1.16699 3.77964 3.77866 1.16797 7.00033 1.16797C10.222 1.16797 12.8337 3.77964 12.8337 7.0013Z"
-        stroke="#7E868C"
-        stroke-width="1.66667"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
-  </Stats>
-);
-
 const actions = (
   <ActionArea>
-    {stats}
     <Widget src={`${ownerId}/widget/CreateNewInput`} />
   </ActionArea>
 );
