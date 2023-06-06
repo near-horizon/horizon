516da03f58914d60ebe0c32071a560f1d0cec8d8
Petar Vujović <petarvujovic98@gmail.com>
2023-06-06 12:37:08 +0200

    fix: Fix fields types

--- scripts/add_investor.sh
+++ scripts/add_investor.sh
@@ -5,4 +5,4 @@ ACCOUNT_ID=$1
 CONTRACT_ID=$2
 SIGNER_ID=$3
 
-NEAR_ENV=mainnet near call $CONTRACT_ID add_investors "{\"investors\":{\"$ACCOUNT_ID\":{\"contact\":\"$ACCOUNT_ID\",\"permissions\":{\"$ACCOUNT_ID\":\"Admin\"}}}}" --accountId $SIGNER_ID
+NEAR_ENV=mainnet near call $CONTRACT_ID add_investors "{\"investors\":{\"$ACCOUNT_ID\":{\"contact\":\"$ACCOUNT_ID\",\"permissions\":{\"$ACCOUNT_ID\":[\"Admin\"]},\"verified\":true}}}" --accountId $SIGNER_ID
