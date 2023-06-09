fcd600b9916e845248770a4f1a7f18932f2397b3
Petar Vujović <petarvujovic98@gmail.com>
2023-06-09 18:39:45 +0200

    fix: Fix responsiveness for narrower screens

--- widgets/src/Home/ListSection.jsx
+++ widgets/src/Home/ListSection.jsx
@@ -98,11 +98,11 @@ const List = styled.div`
       width: 100%;
     }
 
-    @media screen and (max-width: 1024px) {
+    @media screen and (min-width: 768px) and (max-width: 1424px) {
       width: 49%;
     }
 
-    @media screen and (min-width: 1024px) {
+    @media screen and (min-width: 1424px) {
       width: 32%;
     }
   }
--- widgets/src/Project/ListSection.jsx
+++ widgets/src/Project/ListSection.jsx
@@ -107,11 +107,11 @@ const List = styled.div`
       width: 100%;
     }
 
-    @media screen and (max-width: 1024px) {
+    @media screen and (min-width: 768px) and (max-width: 1424px) {
       width: 49%;
     }
 
-    @media screen and (min-width: 1024px) {
+    @media screen and (min-width: 1424px) {
       width: 32%;
     }
   }
