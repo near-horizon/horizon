ae4406db4623907273ce3732cb802cb1ef98dfcc
Petar Vujović <petarvujovic98@gmail.com>
2023-06-26 10:12:07 +0200

    chore: Support running on MacOS

--- dev_widget_deploy.sh
+++ dev_widget_deploy.sh
@@ -6,8 +6,13 @@ NEW_ACCOUNT=$2
 OLD_API_URL=$3
 NEW_API_URL=$4
 
-fd -e jsx -e json -x sed -i "s/$OLD_ACCOUNT/$NEW_ACCOUNT/g" {}
-fd -e jsx -e json -x sed -i "s,$OLD_API_URL,$NEW_API_URL,g" {}
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  fd -e jsx -e json -x sed -i '' -e "s/$OLD_ACCOUNT/$NEW_ACCOUNT/g" {}
+  fd -e jsx -e json -x sed -i '' -e "s,$OLD_API_URL,$NEW_API_URL,g" {}
+else
+  fd -e jsx -e json -x sed -i "s/$OLD_ACCOUNT/$NEW_ACCOUNT/g" {}
+  fd -e jsx -e json -x sed -i "s,$OLD_API_URL,$NEW_API_URL,g" {}
+fi
 
 cd widgets/
 
@@ -15,5 +20,10 @@ npm run deploy
 
 cd ..
 
-fd -e jsx -e json -x sed -i "s/$NEW_ACCOUNT/$OLD_ACCOUNT/g" {}
-fd -e jsx -e json -x sed -i "s,$NEW_API_URL,$OLD_API_URL,g" {}
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  fd -e jsx -e json -x sed -i '' -e "s/$NEW_ACCOUNT/$OLD_ACCOUNT/g" {}
+  fd -e jsx -e json -x sed -i '' -e "s,$NEW_API_URL,$OLD_API_URL,g" {}
+else
+  fd -e jsx -e json -x sed -i "s/$NEW_ACCOUNT/$OLD_ACCOUNT/g" {}
+  fd -e jsx -e json -x sed -i "s,$NEW_API_URL,$OLD_API_URL,g" {}
+fi
