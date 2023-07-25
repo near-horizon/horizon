47bf4468b229fdd2f36d936f090877a2c55a107b
Petar Vujovic <petarvujovic98@gmail.com>
2023-07-25 10:35:12 +0200

    chore: Improve script
    Add account and API URL change

--- scripts/single_widget_deploy.sh
+++ scripts/single_widget_deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -eou pipefail
 
 cd "$(dirname "$0")"
 cd ..
@@ -7,6 +6,9 @@ cd ..
 ACCOUNT=$1
 WIDGET_NAME=$2
 WIDGET_DIR=$(dirname "$WIDGET_NAME")
+API=$3
+OLD_ACCOUNT=$4
+OLD_API=$5
 
 if [ "$WIDGET_DIR" == "." ]; then
 	WIDGET_DIR=""
@@ -16,6 +18,14 @@ mkdir -p .tmp/src/"$WIDGET_DIR"
 cp "widgets/src/$WIDGET_NAME" .tmp/src/"$WIDGET_DIR"
 
 cd .tmp
+
+if [ -n "$OLD_ACCOUNT" ]; then
+  fd -e jsx -e json -x sed -i "s/$OLD_ACCOUNT/$ACCOUNT/g" {}
+fi
+
+if [ -n "$OLD_API" ]; then
+	fd -e jsx -e json -x sed -i "s/$OLD_API/$API/g" {}
+fi
 
 bos components deploy $ACCOUNT sign-as $ACCOUNT network-config mainnet sign-with-access-key-file ~/.near-credentials/mainnet/$ACCOUNT.json send
 
