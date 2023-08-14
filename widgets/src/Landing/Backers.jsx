5285e71f9a78affc12a3734400a48e2ef5cf9166
Petar Vujovic <petarvujovic98@gmail.com>
2023-08-14 10:53:09 +0200

    fix: Align buttons to left

--- widgets/src/Landing/Backers.jsx
+++ widgets/src/Landing/Backers.jsx
@@ -21,7 +21,7 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
     gap: 2.25rem;
   }
 
