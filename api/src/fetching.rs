bd3bd03811f925fc8b5374b65474519b1c2559a8
Petar Vujovic <petarvujovic98@gmail.com>
2023-09-06 13:51:09 +0200

    feat: Add proper field mapping

--- api/src/fetching.rs
+++ api/src/fetching.rs
@@ -4,18 +4,76 @@ use serde::{Deserialize, Serialize};
 use crate::{ApiResult, AppState};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Perk {
-    pub name: String,
-    pub description: String,
-    pub price: u64,
-    pub available: bool,
+pub struct Thumbnail {
     pub url: String,
+    #[serde(default)]
+    pub width: u64,
+    #[serde(default)]
+    pub height: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Thumbnails {
+    pub small: Thumbnail,
+    pub large: Thumbnail,
+    pub full: Thumbnail,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AttachmentObject {
+    pub id: String,
+    pub url: String,
+    pub filename: String,
+    pub size: u64,
+    pub r#type: String,
+    #[serde(default)]
+    pub width: u64,
+    #[serde(default)]
+    pub height: u64,
+    pub thumbnails: Thumbnails,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Perk {
+    #[serde(alias = "Name")]
+    pub name: String,
+    #[serde(alias = "Benefit", default)]
+    pub benefit: String,
+    #[serde(alias = "About", default)]
+    pub description: String,
+    #[serde(alias = "Cost", default)]
+    pub price: u64,
+    #[serde(
+        alias = "Product Readiness",
+        deserialize_with = "deserialize_available"
+    )]
+    pub available: bool,
+    #[serde(alias = "URL", default, skip_serializing)]
+    pub url: String,
+    #[serde(alias = "Claim Instructions", default)]
+    pub instructions: String,
+    #[serde(alias = "Code", default, skip_serializing)]
     pub code: Option<String>,
+    #[serde(alias = "Logo")]
+    pub logo: Vec<AttachmentObject>,
+    #[serde(alias = "Banner")]
+    pub banner: Vec<AttachmentObject>,
+    #[serde(alias = "Category", default)]
+    pub category: Vec<String>,
+}
+
+fn deserialize_available<'de, D>(deserializer: D) -> Result<bool, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    Ok(s == "Yes")
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct PerkResponse {
     pub id: String,
+    #[serde(alias = "createdTime")]
     pub created_time: String,
     pub fields: Perk,
 }
