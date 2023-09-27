8241cc85ded02410c5b620e6d7780ee97695e650
Petar Vujovic <petarvujovic98@gmail.com>
2023-09-27 12:56:27 +0200

    feat: Add local storage sync for forms

--- app/src/lib/mutating.ts
+++ app/src/lib/mutating.ts
@@ -1,3 +1,10 @@
+import { useEffect } from "react";
+import {
+  type Path,
+  type FieldValues,
+  type UseFormReturn,
+  type PathValue,
+} from "react-hook-form";
 import { z } from "zod";
 
 export const progressSchema = z.object({
@@ -6,3 +13,43 @@ export const progressSchema = z.object({
 });
 
 export type Progress = z.infer<typeof progressSchema>;
+
+export function useLocalSaveForm<T extends z.ZodObject<FieldValues>>(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  form: UseFormReturn<z.infer<T>, any, undefined>,
+  schema: T,
+  id: string
+) {
+  const change = form.watch();
+
+  useEffect(() => {
+    const data = readLocalSaveForm(id);
+    if (data && Object.keys(data).length > 0) {
+      Object.entries(data).forEach(([key, value]) => {
+        if (key in schema.shape) {
+          form.setValue(
+            key as Path<z.infer<T>>,
+            value as PathValue<z.infer<T>, Path<z.infer<T>>>
+          );
+        }
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(`form-${id}`, JSON.stringify(change));
+  }, [change, id]);
+}
+
+export function readLocalSaveForm(id: string): unknown {
+  const form = localStorage.getItem(`form-${id}`);
+  if (!form) {
+    return null;
+  }
+  return JSON.parse(form);
+}
+
+export function clearLocalSaveForm(id: string) {
+  localStorage.removeItem(`form-${id}`);
+}
