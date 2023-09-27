617f9d52e0182c4d9ad6612b8230a67b240d7eb9
Petar Vujovic <petarvujovic98@gmail.com>
2023-09-27 12:54:54 +0200

    feat: Add number input and add invalid date prop

--- app/src/components/inputs/date.tsx
+++ app/src/components/inputs/date.tsx
@@ -25,6 +25,7 @@ export function DateInput<
   props: UseControllerProps<TFieldValues, TName> & {
     placeholder?: string;
     description?: string;
+    invalidDates?: (date: Date) => boolean;
   }
 ) {
   return (
@@ -39,27 +40,25 @@ export function DateInput<
                 <Button
                   variant={"outline"}
                   className={cn(
-                    "w-[240px] pl-3 text-left font-normal",
+                    "w-[240px] rounded-md pl-3 text-left font-normal",
                     !field.value && "text-muted-foreground"
                   )}
                 >
                   {field.value ? (
-                    format(field.value, "PPP")
+                    format(new Date(field.value), "PPP")
                   ) : (
                     <span>Pick a date</span>
                   )}
-                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                  <CalendarIcon className="ml-auto h-6 w-6 opacity-50" />
                 </Button>
               </FormControl>
             </PopoverTrigger>
             <PopoverContent className="w-auto p-0" align="start">
               <Calendar
                 mode="single"
-                selected={field.value}
-                onSelect={field.onChange}
-                disabled={(date) =>
-                  date > new Date() || date < new Date("1900-01-01")
-                }
+                selected={new Date(field.value)}
+                onSelect={(date) => field.onChange(date?.toISOString())}
+                disabled={props.invalidDates}
                 initialFocus
               />
             </PopoverContent>
