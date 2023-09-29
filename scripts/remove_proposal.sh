90453303248876ef054437d2bd7abeb4401cf12b
Petar Vujovic <petarvujovic98@gmail.com>
2023-09-29 09:12:48 +0200

    chore: Add remove proposal helper script

---
+++ scripts/remove_proposal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+CONTRACT_ID=$1
+SIGNER_ID=$2
+PROJECT_ID=$3
+VENDOR_ID=$4
+CID=$5
+
+NEAR_ENV=mainnet near call $CONTRACT_ID remove_proposal "{\"project_id\":\"$PROJECT_ID\",\"vendor_id\":\"$VENDOR_ID\",\"cid\":\"$CID\"}" --accountId $SIGNER_ID
