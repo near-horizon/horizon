2bc0cef24381c38bee8a797d6363f140497d0c09
Petar Vujovic <petarvujovic98@gmail.com>
2023-10-05 09:40:34 +0200

    fix: Move session getter into separate file

---
+++ app/src/lib/session.ts
@@ -0,0 +1,16 @@
+import { cookies } from "next/headers";
+import { unsealData } from "iron-session/edge";
+import { ironSessionConfig } from "./constants/iron-session";
+import { type IronSession } from "iron-session";
+
+export async function getUserFromSession() {
+  const cookieStore = cookies();
+
+  const session = cookieStore.get(ironSessionConfig.cookieName)?.value;
+
+  return session
+    ? await unsealData<IronSession["user"]>(session, {
+        password: ironSessionConfig.password,
+      })
+    : null;
+}
