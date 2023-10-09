94391dec5cdc997a5420873a7e33eaea3a149f30
Petar Vujovic <petarvujovic98@gmail.com>
2023-10-09 12:56:50 +0200

    chore: Remove border around checkbox

--- app/src/components/inputs/checkbox.tsx
+++ app/src/components/inputs/checkbox.tsx
@@ -21,7 +21,7 @@ export function CheckboxInput<
     <FormField
       {...props}
       render={({ field }) => (
-        <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+        <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border-0 p-4">
           <FormControl>
             <Checkbox checked={field.value} onCheckedChange={field.onChange} />
           </FormControl>
