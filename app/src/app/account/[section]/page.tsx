28527755065e032abdaf8603998258137e9739c3
Chris <cdaracena@gmail.com>
2023-10-11 09:07:13 -0400

    Added new GET route for growth programs, updated file / folder locations related to growth-programs

--- app/src/app/account/[section]/page.tsx
+++ app/src/app/account/[section]/page.tsx
@@ -1,13 +1,7 @@
-import GrowthPrograms from "./growth";
 export default function ProfileSection({
   params: { section },
 }: {
   params: { section: string };
 }) {
-  switch (section) {
-    case "growth":
-      return <GrowthPrograms />;
-    default:
-      return section;
-  }
+  return section;
 }
