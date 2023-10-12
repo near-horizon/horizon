45c3a466880a12b713eb782b309f60f451f6069b
Petar Vujovic <petarvujovic98@gmail.com>
2023-10-12 16:59:45 +0200

    feat: Add streaming to list pages

--- app/src/app/requests/(with-header-layout)/layout.tsx
+++ app/src/app/requests/(with-header-layout)/layout.tsx
@@ -1,9 +1,29 @@
 import { ListPageLayout } from "~/app/list-page-layout";
+import { RequestSkeleton } from "../card";
+import { pageSize } from "~/lib/constants/pagination";
 
 export default function RequestsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <ListPageLayout title="Requests">{children}</ListPageLayout>;
+  return (
+    <ListPageLayout
+      title="Requests"
+      fallback={
+        <ul className="flex flex-row flex-wrap items-stretch justify-start gap-2">
+          {[...Array(pageSize).keys()].map((item) => (
+            <li
+              key={item}
+              className="w-full md:w-[calc((100%-.5rem)*.5)] lg:w-[calc((100%-1rem)*.33)] 2xl:w-[calc((100%-1.5rem)*.25)]"
+            >
+              <RequestSkeleton />
+            </li>
+          ))}
+        </ul>
+      }
+    >
+      {children}
+    </ListPageLayout>
+  );
 }
