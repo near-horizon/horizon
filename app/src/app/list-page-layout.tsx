80622ba65765a6cea9d3824f8fb08c0a8d79f207
Petar Vujovic <petarvujovic98@gmail.com>
2023-10-12 16:59:45 +0200

    feat: Add streaming to list pages

--- app/src/app/list-page-layout.tsx
+++ app/src/app/list-page-layout.tsx
@@ -3,14 +3,18 @@ import { Suspense } from "react";
 export function ListPageLayout({
   title,
   children,
+  fallback,
 }: {
   title: string;
   children?: React.ReactNode;
+  fallback?: React.ReactNode;
 }) {
   return (
     <div className="flex flex-col items-center gap-8">
       <h1 className="w-full text-3xl font-semibold">{title}</h1>
-      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+      <Suspense fallback={fallback ?? <div>Loading...</div>}>
+        {children}
+      </Suspense>
     </div>
   );
 }
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,36 @@
-import { ListPageLayout } from "./list-page-layout";
-import { Home } from "./home";
+import { MainStatsSkeleton } from "~/components/main-stats";
+import { ProjectsListSectionLoader } from "./projects/loader";
+import { Suspense } from "react";
+import { ProjectsListSectionSkeleton } from "./projects/list";
+import { ContributorsListSectionSkeleton } from "./contributors/list";
+import { ContributorsListSectionLoader } from "./contributors/loader";
+import { BackersListSectionSkeleton } from "./backers/list";
+import { BackersListSectionLoader } from "./backers/loader";
+import { RequestsListSectionSkeleton } from "./requests/list";
+import { RequestsListSectionLoader } from "./requests/loader";
+import { StatsLoader } from "./stats-loader";
 
-export default function HomePage() {
+export default function Home() {
   return (
-    <ListPageLayout title="Explore NEAR Horizon">
-      <Home />
-    </ListPageLayout>
+    <div className="flex flex-col items-center gap-8">
+      <h1 className="w-full text-3xl font-semibold">Explore NEAR Horizon</h1>
+      <div className="flex flex-col gap-8">
+        <Suspense fallback={<MainStatsSkeleton />}>
+          <StatsLoader />
+        </Suspense>
+        <Suspense fallback={<ProjectsListSectionSkeleton />}>
+          <ProjectsListSectionLoader />
+        </Suspense>
+        <Suspense fallback={<RequestsListSectionSkeleton />}>
+          <RequestsListSectionLoader />
+        </Suspense>
+        <Suspense fallback={<ContributorsListSectionSkeleton />}>
+          <ContributorsListSectionLoader />
+        </Suspense>
+        <Suspense fallback={<BackersListSectionSkeleton />}>
+          <BackersListSectionLoader />
+        </Suspense>
+      </div>
+    </div>
   );
 }
