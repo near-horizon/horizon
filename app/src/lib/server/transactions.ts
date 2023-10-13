61b807200b44200e5cdc09ba70eb46bab2b6d020
Petar Vujovic <petarvujovic98@gmail.com>
2023-10-13 14:34:13 +0200

    refactor: Remove unneccessary value

--- app/src/lib/server/transactions.ts
+++ app/src/lib/server/transactions.ts
@@ -3,15 +3,11 @@ import { type Transaction } from "../validation/common";
 import { type Stats, statsSchema } from "../validation/fetching";
 
 export async function getTransactions(): Promise<Transaction[]> {
-  const projects = await fetch(env.API_URL + "/transactions/all", {
-    cache: "default",
-  });
+  const projects = await fetch(env.API_URL + "/transactions/all");
   return projects.json() as Promise<Transaction[]>;
 }
 
 export async function getStats(): Promise<Stats> {
-  const projects = await fetch(env.API_URL + "/transactions/stats", {
-    cache: "default",
-  });
+  const projects = await fetch(env.API_URL + "/transactions/stats");
   return statsSchema.parse(await projects.json());
 }
