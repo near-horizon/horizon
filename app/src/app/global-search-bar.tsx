50a401e83038c5c66dcb9950a6c70e91aa54ff21
Petar Vujovic <petarvujovic98@gmail.com>
2023-10-17 14:43:29 +0200

    feat: Add on enter capture

--- app/src/app/global-search-bar.tsx
+++ app/src/app/global-search-bar.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { SearchInput } from "~/components/inputs/search";
 import { Button } from "~/components/ui/button";
 import { cn } from "~/lib/utils";
 
 export function GlobalSearchBar() {
-  const [searchQuery, setSearchQuery] = useState<string>("");
+  const searchParams = useSearchParams();
+  const q = searchParams.get("q") ?? "";
+  const [searchQuery, setSearchQuery] = useState<string>(q);
   const router = useRouter();
 
   return (
     <div
       className={cn(
         "flex flex-row items-stretch justify-start rounded-full border border-ui-elements-gray bg-background-white",
-        "py-0 focus-within:ring "
+        "py-0 shadow focus-within:ring"
       )}
     >
       <SearchInput
@@ -22,6 +24,9 @@ export function GlobalSearchBar() {
         setValue={setSearchQuery}
         className="rounded-r-none border-none shadow-none focus-within:ring-0"
         placeholder="Search..."
+        onEnter={() => {
+          router.push(`/search?q=${searchQuery}`);
+        }}
       />
       <Button
         variant="default"
