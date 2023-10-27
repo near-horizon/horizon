937977b54898bf90af4ba82bed4b4c86b25ad99f
Chris <cdaracena@gmail.com>
2023-10-27 12:58:44 -0400

    Added framer motion, created join horizon/ build row, updated svgs, added shadcn custom accordion component

--- web/tailwind.config.mjs
+++ web/tailwind.config.mjs
@@ -1,13 +1,67 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{astro,html,md,mdx,ts,tsx}"],
+module.exports = {
+  darkMode: ["class"],
+  content: ["./src/**/*.{ts,tsx,astro}"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         foreground: "hsl(var(--foreground))",
+        gradient: {
+          12: "var(--12)",
+          3: "var(--3)",
+          dawn: "var(--dawn)",
+          "dawn-2": "var(--dawn-2)",
+        },
+        "ui-elements": {
+          light: "var(--ui-elements--light)",
+          gray: "var(--ui-elements--gray)",
+          dark: "var(--ui-elements--dark)",
+          white: "var(--ui-elements--white)",
+          black: "var(--ui-elements--black)",
+        },
+        background: {
+          light: "var(--background--light)",
+          darker: "var(--background--darker)",
+          dark: "var(--background--dark)",
+          beige: "var(--background--beige)",
+          white: "var(--background--white)",
+          black: "var(--background--black)",
+          "black-2": "var(--background--black-2)",
+          DEFAULT: "hsl(var(--background))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        error: {
+          focus: "var(--error--error-focus)",
+          pressed: "var(--error--error-pressed)",
+          hover: "var(--error--error-hover)",
+          DEFAULT: "var(--error--error-default)",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          disabled: "var(--accent--accent-disabled)",
+          link: "var(--accent--accent-link)",
+          focus: "var(--accent--accent-focus)",
+          pressed: "var(--accent--accent-pressed)",
+          hover: "var(--accent--accent-hover)",
+          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: "var(--accent--accent-default)",
+        },
         primary: {
           disabled: "var(--primary--primary-disabled)",
           link: "var(--primary--primary-link)",
@@ -27,22 +81,9 @@ export default {
           DEFAULT: "var(--secondary--secondary-default)",
           foreground: "hsl(var(--secondary-foreground))",
         },
-        "ui-elements": {
-          light: "var(--ui-elements--light)",
-          gray: "var(--ui-elements--gray)",
-          dark: "var(--ui-elements--dark)",
-          white: "var(--ui-elements--white)",
-          black: "var(--ui-elements--black)",
-        },
-        background: {
-          light: "var(--background--light)",
-          darker: "var(--background--darker)",
-          dark: "var(--background--dark)",
-          beige: "var(--background--beige)",
-          white: "var(--background--white)",
-          black: "var(--background--black)",
-          "black-2": "var(--background--black-2)",
-          DEFAULT: "hsl(var(--background))",
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
         text: {
           error: "var(--text--text-error)",
@@ -54,8 +95,34 @@ export default {
           white: "var(--text--text-white)",
           link: "var(--text--text-link)",
         },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/container-queries"),
+  ],
 };
