c79fce502fa2eae218af23c17ad0745becd8f33e
Chris <cdaracena@gmail.com>
2023-10-27 12:58:44 -0400

    Added framer motion, created join horizon/ build row, updated svgs, added shadcn custom accordion component

---
+++ web/src/components/landing-page/BuildSection.tsx
@@ -0,0 +1,186 @@
+import { useState } from "react";
+
+import { motion } from "framer-motion";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+} from "@components/ui/accordion";
+
+import CustomAccordionTrigger from "@components/ui/custom/CustomAccordionTrigger";
+
+import orangeArrowSvgRef from "@assets/orange-arrow.svg";
+import soonPillSvgRef from "@assets/soon-pill.svg";
+import lightbulbSvgRef from "@assets/lightbulb.svg";
+import boxCheckSvgRef from "@assets/box-check.svg";
+import compassSvgRef from "@assets/compass.svg";
+
+import { cn } from "@lib/utils";
+
+enum ViewOptions {
+  HIRE = "hire",
+  VALIDATE = "validate",
+  MENTOR = "mentor",
+}
+
+const build = {
+  [ViewOptions.HIRE]: {
+    desktop: "bafkreia4if3v5pidg5wn3ajuox7novoqzabtgqjcxtutxscip5dmrhrcni",
+    mobile: "bafkreid3i5zlxu2eiqca7qghpgpoj2zbmp2kwv7qtkzdj2utyqhunk56y4",
+  },
+  [ViewOptions.VALIDATE]: {
+    desktop: "bafkreiaj66jfkq67dtohhhyugu6fzvwes5iojhnn6552qtsyiyg33lv35e",
+    mobile: "bafkreigc2hjgoma3gewa55buw4t3ueoshwap25iu2t6hcyapjwvpqo2qfi",
+  },
+  [ViewOptions.MENTOR]: {
+    desktop: "bafkreicichhd7bqfhvwphgb3oeo6rv2eritrqhdinaabam2c6xo7epju5e",
+    mobile: "bafkreigs2ktn7dokv4l6nug733ovyzqco7lw5hvqluhwzst2sgce3ty4dy",
+  },
+};
+
+export default function BuildSection() {
+  const [currentView, setCurrentView] = useState<ViewOptions>(ViewOptions.HIRE);
+
+  const onCurrentViewChange = (selectedView: ViewOptions) =>
+    setCurrentView(selectedView);
+
+  const mapImage = (src: string) => `https://ipfs.near.social/ipfs/${src}`;
+
+  return (
+    <div className="flex flex-col md:flex-row md:justify-between items-center gap-12">
+      <div className="flex-1 flex flex-col gap-5">
+        <h2 className="flex justify-center  items-center self-stretch md:justify-start gap-2">
+          <span className="text-background-dark font-bold text-3xl hidden md:block">
+            Join Horizon
+          </span>
+
+          <img
+            src={orangeArrowSvgRef.src}
+            className="w-9 h-3 hidden md:block"
+          />
+          <span className="text-[#ff7966] font-bold text-3xl">Build</span>
+        </h2>
+        <Accordion
+          type="single"
+          onValueChange={onCurrentViewChange}
+          defaultValue={ViewOptions.HIRE}
+          value={currentView}
+        >
+          <AccordionItem
+            value={ViewOptions.HIRE}
+            className={cn(
+              "border-none px-6 pt-3 pb-4",
+              currentView === ViewOptions.HIRE &&
+                "rounded-lg shadow-[-8px_40px_59px_-28px_rgba(16,24,40,0.14),-2px_2px_14px_-1px_rgba(0,0,0,0.13)]"
+            )}
+          >
+            <CustomAccordionTrigger className="hover:no-underline gap-2 justify-start">
+              <img src={boxCheckSvgRef.src} className="h-8 w-8" />
+              <h3 className="text-lg font-bold">
+                Hire ecosystem experts for your needs
+              </h3>
+            </CustomAccordionTrigger>
+            <AccordionContent className="text-sm font-normal text-black pl-11">
+              Easily source reputable contributors for any and every business
+              need you have, including back office finance management,
+              recruitment, development, legal, and marketing.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem
+            value={ViewOptions.VALIDATE}
+            className={cn(
+              "border-none px-6 pt-3 pb-4",
+              currentView === ViewOptions.VALIDATE &&
+                "rounded-lg shadow-[-8px_40px_59px_-28px_rgba(16,24,40,0.14),-2px_2px_14px_-1px_rgba(0,0,0,0.13)]"
+            )}
+          >
+            <CustomAccordionTrigger className="hover:no-underline gap-2 justify-start">
+              <img src={lightbulbSvgRef.src} className="h-9 w-9" />
+              <h3 className="text-lg font-bold">Validate your idea safely</h3>
+              <img src={soonPillSvgRef.src} className="w-11 h-3" />
+            </CustomAccordionTrigger>
+            <AccordionContent className="text-sm font-normal text-black pl-11">
+              Users are standing by to test your application and provide
+              valuable feedback before your launch.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem
+            value={ViewOptions.MENTOR}
+            className={cn(
+              "border-none px-6 pt-3 pb-4",
+              currentView === ViewOptions.MENTOR &&
+                "rounded-lg shadow-[-8px_40px_59px_-28px_rgba(16,24,40,0.14),-2px_2px_14px_-1px_rgba(0,0,0,0.13)]"
+            )}
+          >
+            <CustomAccordionTrigger className="hover:no-underline gap-2 justify-start">
+              <img src={compassSvgRef.src} className="h-9 w-9" />
+              <h3 className="text-lg font-bold">Find a mentor or become one</h3>
+              <img src={soonPillSvgRef.src} className="w-11 h-3" />
+            </CustomAccordionTrigger>
+            <AccordionContent className="text-sm font-normal text-black pl-11">
+              Connect with industry veterans for advice on your GTM plan,
+              leadership skills, pitch deck, and more. Or apply to become a
+              mentor, and help Web3 founders put their best foot forward as they
+              prepare for launch.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+
+      <motion.div
+        variants={{
+          [ViewOptions.HIRE]: { opacity: 1, scale: 1 },
+          [ViewOptions.VALIDATE]: { opacity: 1, scale: 1 },
+          [ViewOptions.MENTOR]: { opacity: 1, scale: 1 },
+        }}
+        className="flex-1"
+        initial={ViewOptions.HIRE}
+        animate={currentView}
+      >
+        {currentView === ViewOptions.HIRE && (
+          <img
+            src={mapImage(build[ViewOptions.HIRE].desktop)}
+            alt="hire view"
+            className="hidden md:block"
+          />
+        )}
+        {currentView === ViewOptions.HIRE && (
+          <img
+            src={mapImage(build[ViewOptions.HIRE].mobile)}
+            alt="hire view"
+            className="md:hidden"
+          />
+        )}
+        {currentView === ViewOptions.VALIDATE && (
+          <img
+            src={mapImage(build[ViewOptions.VALIDATE].desktop)}
+            alt="validate view"
+            className="hidden md:block"
+          />
+        )}
+        {currentView === ViewOptions.VALIDATE && (
+          <img
+            src={mapImage(build[ViewOptions.VALIDATE].mobile)}
+            alt="validate view"
+            className="md:hidden"
+          />
+        )}
+        {currentView === ViewOptions.MENTOR && (
+          <img
+            src={mapImage(build[ViewOptions.MENTOR].desktop)}
+            alt="mentor view"
+            className="hidden md:block"
+          />
+        )}
+        {currentView === ViewOptions.MENTOR && (
+          <img
+            src={mapImage(build[ViewOptions.MENTOR].mobile)}
+            alt="mentor view"
+            className="md:hidden"
+          />
+        )}
+      </motion.div>
+    </div>
+  );
+}
