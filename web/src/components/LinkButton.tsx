54e0452c4b9c449b79955e4e394502b778a5210d
Chris <cdaracena@gmail.com>
2023-10-30 11:34:44 -0400

    Updated SVG Components, updated Backers, Connect, Contributors, Partners, Testimonial and Grow sections

--- web/src/components/LinkButton.tsx
+++ web/src/components/LinkButton.tsx
@@ -4,19 +4,22 @@ interface LinkButtonProps
   extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   label: string;
   variant?: "primary" | "secondary";
+  className?: string;
 }
 
 export function LinkButton({
   label,
   variant = "primary",
   href,
+  className,
   ...props
 }: LinkButtonProps) {
   return (
     <a
       className={cn(
         "px-7 py-3 rounded-full text-center text-text-dark font-semibold text-lg w-fit",
-        variant === "secondary" ? "border-2 border-black" : "bg-primary"
+        variant === "secondary" ? "border-2 border-black" : "bg-primary",
+        className
       )}
       href={href}
       {...props}
