c8e1e9c539650ebdce37da4ae0e3ad0bd67fc659
Petar Vujovic <petarvujovic98@gmail.com>
2023-10-31 11:59:14 +0100

    fix: Check for token before redirect

--- app/src/app/projects/[accountId]/backers-digest/page.tsx
+++ app/src/app/projects/[accountId]/backers-digest/page.tsx
@@ -14,12 +14,12 @@ export default async function BackersDigestPage({
 }) {
   const user = await getUserFromSession();
 
-  if (!user) {
+  if (!user || !token || token === "") {
     return redirect(`/projects/${accountId}/overview`);
   }
 
   const isBacker = await hasBacker(user.accountId);
-  const backersDigest = await getBackersDigest(user.accountId);
+  const backersDigest = await getBackersDigest(accountId);
   const hasPermission =
     ((isBacker || user.accountId === accountId) && backersDigest.published) ??
     token === backersDigest.token;
