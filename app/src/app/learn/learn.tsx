3d1d938f1484ba31d66d32380317cb20ec37f504
Petar Vujovic <petarvujovic98@gmail.com>
2023-11-02 15:03:28 +0100

    fix: Update padding on main pages

--- app/src/app/learn/learn.tsx
+++ app/src/app/learn/learn.tsx
@@ -49,82 +49,88 @@ export function Learn() {
   );
 
   return (
-    <div className="flex w-full max-w-[max(calc(100%-1rem),1536px)] flex-col gap-8 2xl:mx-auto">
-      <div className="mb-6 text-3xl font-bold tracking-wide text-gray-900">
-        Learning resources{" "}
-        <span className="text-2xl font-normal text-ui-elements-gray">
-          {totalResourcesCount}
-        </span>
-      </div>
-      <div>
-        <div className="flex gap-2">
-          <FilterDropdown
-            triggerText="Category"
-            options={tagFilterOptions}
-            onChange={setSelectedCategories}
-            selected={selectedCategories}
-            getFilteredCount={(selected) =>
-              filterData(selected, selectedTypes, searchText).reduce(
-                (acc: number, resourceItem: LearningCategory) =>
-                  acc + resourceItem.items.length,
-                0
-              )
-            }
-          />
-          <FilterDropdown
-            triggerText="Type"
-            options={typeFilterOptions}
-            onChange={setSelectedTypes}
-            selected={selectedTypes}
-            getFilteredCount={(selected) =>
-              filterData(selectedCategories, selected, searchText).reduce(
-                (acc: number, resourceItem: LearningCategory) =>
-                  acc + resourceItem.items.length,
-                0
-              )
-            }
-          />
+    <div className="flex flex-col items-center gap-8 rounded-xl border border-ui-elements-light bg-background-white p-12 pt-6 shadow">
+      <div className="flex w-full max-w-[max(calc(100%-1rem),1536px)] flex-col gap-8 2xl:mx-auto">
+        <div className="mb-6 text-3xl font-bold tracking-wide text-gray-900">
+          Learning resources{" "}
+          <span className="text-2xl font-normal text-ui-elements-gray">
+            {totalResourcesCount}
+          </span>
         </div>
-        {/* Quick filters will go here */}
-        <div className="mt-8 flex items-center justify-between">
-          <div className="text-sm text-ui-elements-dark">
-            <span className="font-bold">
-              {filterData(selectedCategories, selectedTypes, searchText).reduce(
-                (acc: number, resourceItem: LearningCategory) =>
-                  acc + resourceItem.items.length,
-                0
-              )}{" "}
-            </span>
-            <span className="font-normal">results</span>
+        <div>
+          <div className="flex gap-2">
+            <FilterDropdown
+              triggerText="Category"
+              options={tagFilterOptions}
+              onChange={setSelectedCategories}
+              selected={selectedCategories}
+              getFilteredCount={(selected) =>
+                filterData(selected, selectedTypes, searchText).reduce(
+                  (acc: number, resourceItem: LearningCategory) =>
+                    acc + resourceItem.items.length,
+                  0
+                )
+              }
+            />
+            <FilterDropdown
+              triggerText="Type"
+              options={typeFilterOptions}
+              onChange={setSelectedTypes}
+              selected={selectedTypes}
+              getFilteredCount={(selected) =>
+                filterData(selectedCategories, selected, searchText).reduce(
+                  (acc: number, resourceItem: LearningCategory) =>
+                    acc + resourceItem.items.length,
+                  0
+                )
+              }
+            />
           </div>
+          {/* Quick filters will go here */}
+          <div className="mt-8 flex items-center justify-between">
+            <div className="text-sm text-ui-elements-dark">
+              <span className="font-bold">
+                {filterData(
+                  selectedCategories,
+                  selectedTypes,
+                  searchText
+                ).reduce(
+                  (acc: number, resourceItem: LearningCategory) =>
+                    acc + resourceItem.items.length,
+                  0
+                )}{" "}
+              </span>
+              <span className="font-normal">results</span>
+            </div>
 
-          <SearchInput
-            value={searchText}
-            placeholder="Search resources"
-            setValue={(inputVal) => setSearchText(inputVal)}
-          />
+            <SearchInput
+              value={searchText}
+              placeholder="Search resources"
+              setValue={(inputVal) => setSearchText(inputVal)}
+            />
+          </div>
         </div>
-      </div>
-      <div>
-        {filterData(selectedCategories, selectedTypes, searchText).map(
-          (resource: LearningCategory, i: number) => {
-            return (
-              <div key={resource.id} className={`${i !== 0 ? "mt-14" : ""}`}>
-                <h3 className="mb-8 text-2xl font-bold">{resource.title}</h3>
-                <div className="flex flex-row flex-wrap items-stretch justify-start gap-8">
-                  {resource.items.map((resource) => (
-                    <div
-                      key={resource.title}
-                      className="md:w-[calc((100%-2rem)/2)] xl:w-[calc((100%-4rem)/3)] 2xl:w-[calc((100%-6rem)/4)]"
-                    >
-                      <LearnCard resource={resource} />
-                    </div>
-                  ))}
+        <div>
+          {filterData(selectedCategories, selectedTypes, searchText).map(
+            (resource: LearningCategory, i: number) => {
+              return (
+                <div key={resource.id} className={`${i !== 0 ? "mt-14" : ""}`}>
+                  <h3 className="mb-8 text-2xl font-bold">{resource.title}</h3>
+                  <div className="flex flex-row flex-wrap items-stretch justify-start gap-8">
+                    {resource.items.map((resource) => (
+                      <div
+                        key={resource.title}
+                        className="md:w-[calc((100%-2rem)/2)] xl:w-[calc((100%-4rem)/3)] 2xl:w-[calc((100%-6rem)/4)]"
+                      >
+                        <LearnCard resource={resource} />
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            );
-          }
-        )}
+              );
+            }
+          )}
+        </div>
       </div>
     </div>
   );
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -12,7 +12,7 @@ import { StatsLoader } from "./stats-loader";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center gap-8">
+    <div className="flex flex-col items-center gap-8 rounded-xl border border-ui-elements-light bg-background-white p-12 pt-6 shadow">
       <h1 className="w-full text-3xl font-semibold">Explore NEAR Horizon</h1>
       <div className="flex w-full flex-col gap-8">
         <Suspense fallback={<MainStatsSkeleton />}>
