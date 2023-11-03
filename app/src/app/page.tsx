19a213de8da51126ce2ed5c822d484859e3fe678
Petar Vujović <petarvujovic98@gmail.com>
2023-11-03 17:42:02 +0100

    feat: Update loading for home page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,32 +1,43 @@
-import { MainStatsSkeleton } from "~/components/main-stats";
-import { ProjectsListSectionLoader } from "./projects/loader";
-import { Suspense } from "react";
-import { ProjectsListSectionSkeleton } from "./projects/list";
 import {
-  ContributorsListSection /* , ContributorsListSectionSkeleton  */,
+  MainStats,
+  // MainStatsSkeleton,
+} from "~/components/main-stats";
+// import { ProjectsListSectionLoader } from "./projects/loader";
+// import { Suspense } from "react";
+import {
+  ProjectsListSection,
+  // ProjectsListSectionSkeleton,
+} from "./projects/list";
+import {
+  ContributorsListSection,
+  // ContributorsListSectionSkeleton,
 } from "./contributors/list";
 // import { ContributorsListSectionLoader } from "./contributors/loader";
 import {
-  BackersListSection /* , BackersListSectionSkeleton */,
+  BackersListSection,
+  // BackersListSectionSkeleton,
 } from "./backers/list";
 // import { BackersListSectionLoader } from "./backers/loader";
 import {
-  RequestsListSection /* , RequestsListSectionSkeleton */,
+  RequestsListSection,
+  // RequestsListSectionSkeleton,
 } from "./requests/list";
 // import { RequestsListSectionLoader } from "./requests/loader";
-import { StatsLoader } from "./stats-loader";
+// import { StatsLoader } from "./stats-loader";
+import { getStats } from "~/lib/server/transactions";
 
-export default function Home() {
+export default async function Home() {
+  const { projects, requests, contributors, backers } = await getStats();
   return (
     <div className="flex flex-col items-center gap-8 rounded-xl border border-ui-elements-light bg-background-white p-12 pt-6 shadow">
       <h1 className="w-full text-3xl font-semibold">Explore NEAR Horizon</h1>
       <div className="flex w-full flex-col gap-8">
-        <Suspense fallback={<MainStatsSkeleton />}>
-          <StatsLoader />
-        </Suspense>
-        <Suspense fallback={<ProjectsListSectionSkeleton />}>
-          <ProjectsListSectionLoader />
-        </Suspense>
+        {/* <Suspense fallback={<MainStatsSkeleton />}> */}
+        {/*   <StatsLoader /> */}
+        {/* </Suspense> */}
+        {/* <Suspense fallback={<ProjectsListSectionSkeleton />}> */}
+        {/*   <ProjectsListSectionLoader /> */}
+        {/* </Suspense> */}
         {/* <Suspense fallback={<RequestsListSectionSkeleton />}> */}
         {/*   <RequestsListSectionLoader /> */}
         {/* </Suspense> */}
@@ -36,9 +47,11 @@ export default function Home() {
         {/* <Suspense fallback={<BackersListSectionSkeleton />}> */}
         {/*   <BackersListSectionLoader /> */}
         {/* </Suspense> */}
-        <RequestsListSection count={0} />
-        <ContributorsListSection count={0} />
-        <BackersListSection count={0} />
+        <MainStats />
+        <ProjectsListSection count={projects} />
+        <RequestsListSection count={requests} />
+        <ContributorsListSection count={contributors} />
+        <BackersListSection count={backers} />
       </div>
     </div>
   );
