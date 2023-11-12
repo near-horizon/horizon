8943ac5fbb8dce728e89c4ec4e81d7c37aaaa606
Petar Vujović <petarvujovic98@gmail.com>
2023-11-12 10:33:59 +0000

    feat: Add HZN page

---
+++ web/src/components/hzn/Comprehensive.tsx
@@ -0,0 +1,118 @@
+import { Learning } from "./svgs/learning.js";
+import { Expert } from "./svgs/expert.js";
+import { Fundraising } from "./svgs/fundraising.js";
+import { Discovery } from "./svgs/discovery.js";
+import { Networking } from "./svgs/networking.js";
+import { Access } from "./svgs/access.js";
+import { Earn } from "./svgs/earn.js";
+import { AI } from "./svgs/ai.js";
+import { Product } from "./svgs/product.js";
+import { Marketplace } from "./svgs/marketplace.js";
+import { Curated } from "./svgs/curated.js";
+
+const benefits = [
+  {
+    icon: <Learning className="w-8 h-8" />,
+    heading: "Cohort-based Learning",
+    text: "Benefit from peer-to-peer learning, collaboration, and support, all facilitated by NEAR's experienced community managers.",
+  },
+  {
+    icon: <Expert className="w-8 h-8" />,
+    heading: "Expert-led Sessions",
+    text: "We offer exclusive masterclasses with industry leaders like Illia Polosukhin (NEAR), Paul Hsu (Decasonic), Richard Muirhead (Fabric Ventures), and Michael Kelly (Open Forest Protocol)",
+  },
+  {
+    icon: <Fundraising className="w-8 h-8" />,
+    heading: "Fundraising Track",
+    text: "Get your startup investment-ready with personalized guidance on fundraising strategies, investor relations, pitch preparation, and leadership development",
+  },
+  {
+    icon: <Discovery className="w-8 h-8" />,
+    heading: "'Discovery' Track",
+    text: "Collaborate with our dedicated team to fast-track customer insights, early feedback, and user-centric strategies.",
+  },
+  {
+    icon: <Networking className="w-8 h-8" />,
+    heading: "Networking Opportunities",
+    text: "Join exclusive in-person founder retreats, attend local meetups, and participate in virtual co-working days for a blend of online and IRL engagement.",
+  },
+  {
+    icon: <Access className="w-8 h-8" />,
+    heading: "Access Exclusive Perks",
+    text: "Claim valuable offers from our 20+ vendor partners, including discounts, credits, and free trials tailored to Web3 startups.",
+  },
+  {
+    icon: <Earn className="w-8 h-8" />,
+    heading: "Earn Horizon Credits",
+    text: "Earn Horizon Credits by contributing to the community and participating in events and exchange them for essential services, resources, and access to premier industry experts.",
+  },
+  {
+    icon: <AI className="w-8 h-8" />,
+    heading: "AI Assisted Application Writer",
+    text: "This feature provides suggestions to application forms for grants, accelerators, and incubators, leveraging AI to make the process more efficient and approachable.",
+  },
+  {
+    icon: <Product className="w-8 h-8" />,
+    heading: "Product Audits from UX Experts",
+    text: "These audits will assess the quality of your user research, product design, and provide valuable recommendations to enhance your user journey.",
+  },
+  {
+    icon: <Marketplace className="w-8 h-8" />,
+    heading: "Access to a Vetted Marketplace",
+    text: "This marketplace facilitates reliable access to key services — from legal counsel and marketing specialists to UX and technology consultants",
+  },
+  {
+    icon: <Curated className="w-8 h-8" />,
+    heading: "Curated Learning Resources",
+    text: "Quickly find the resources you and your team need to level up on areas critical to growth and success",
+  },
+];
+
+export function Comprehensive() {
+  return (
+    <div className="flex flex-col items-center gap-7 w-full">
+      <h2 className="text-ui-elements-dark text-center text-4xl font-bold">
+        HZN fellows get comprehensive benefits
+      </h2>
+      <p className="text-background-dark text-center text-xl">
+        HZN is an exclusive learning community for founders that addresses the
+        unique challenges and opportunities within the web3 space. What we're
+        offering is more than just another program; it's a roadmap tailored for
+        Web3 founders.
+      </p>
+      <div className="flex flex-col-reverse gap-6 md:flex-row items-start justify-start flex-wrap md:gap-2">
+        {benefits.map(({ icon, heading, text }) => (
+          <div className="md:w-[calc((100%-4rem)/3)] flex px-6 flex-col items-center gap-4 w-full">
+            {icon}
+            <div className="flex flex-col items-center gap-2 w-full">
+              <h3 className="text-ui-elements-dark text-center text-xl font-bold">
+                {heading}
+              </h3>
+              <p className="text-ui-elements-dark">{text}</p>
+            </div>
+          </div>
+        ))}
+        <div className="flex py-5 px-6 flex-col items-center justify-center gap-3 self-stretch rounded-3xl border-dashed border border-ui-elements-light">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="25"
+            viewBox="0 0 24 25"
+            fill="none"
+          >
+            <path
+              d="M12 5.96777V19.9678M5 12.9678H19"
+              stroke="#98A2B3"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+          <span className="text-center font-medium text-lg">
+            Even more benefits are coming
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ web/src/components/hzn/svgs/learning.tsx
@@ -0,0 +1,56 @@
+export function Learning({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 33"
+      fill="none"
+      className={className}
+    >
+      <g clip-path="url(#clip0_5474_35586)">
+        <rect
+          x="1"
+          y="6"
+          width="14"
+          height="7"
+          rx="1"
+          stroke="#66A0FF"
+          stroke-width="2.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></rect>
+        <rect
+          x="8"
+          y="13"
+          width="15"
+          height="7"
+          rx="1"
+          stroke="#66A0FF"
+          stroke-width="2.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></rect>
+        <rect
+          x="15"
+          y="20"
+          width="16"
+          height="7"
+          rx="1"
+          stroke="#66A0FF"
+          stroke-width="2.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></rect>
+      </g>
+      <defs>
+        <clipPath id="clip0_5474_35586">
+          <rect
+            width="32"
+            height="32"
+            fill="white"
+            transform="translate(0 0.967773)"
+          ></rect>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
