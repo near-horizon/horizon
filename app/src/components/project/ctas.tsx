14a007b038ecc95ec319ac446d1ee08b7bd30e1e
Petar Vujovic <petarvujovic98@gmail.com>
2023-11-15 15:49:21 +0100

    chore: Remove unused components

--- app/src/components/project/ctas.tsx
+++ app/src/components/project/ctas.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import clsx from "clsx";
 import React from "react";
-// import { useProject } from "~/lib/projects";
-import { useWalletSelector } from "~/stores/global";
+import { cn } from "~/lib/utils";
 
 export function CTA({
   onClick,
@@ -18,7 +16,7 @@ export function CTA({
 }) {
   return (
     <button
-      className={clsx(
+      className={cn(
         "flex flex-row items-center justify-center gap-2 whitespace-nowrap rounded-2xl border px-3 py-1 font-medium transition duration-300",
         color === "green"
           ? "border-green-400 bg-green-400 text-[#11181c]"
@@ -32,114 +30,9 @@ export function CTA({
   );
 }
 
-export function CTAs(/* { accountId }: { accountId: AccountId } */) {
-  // const { data, status } = useProject(accountId);
-  const selector = useWalletSelector();
-  const isSignedIn = selector?.isSignedIn();
-
-  const signedInActions = isSignedIn ? (
-    <>
-      <CTA
-        color="green"
-        icon={
-          <svg
-            width="18"
-            height="18"
-            viewBox="0 0 18 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M12 4.5L14.25 2.25M14.25 2.25L16.5 4.5M14.25 2.25V6.75M12 15.75V14.85C12 13.5899 12 12.9598 11.7548 12.4785C11.539 12.0552 11.1948 11.711 10.7715 11.4952C10.2902 11.25 9.66012 11.25 8.4 11.25H5.1C3.83988 11.25 3.20982 11.25 2.72852 11.4952C2.30516 11.711 1.96095 12.0552 1.74524 12.4785C1.5 12.9598 1.5 13.5899 1.5 14.85V15.75M9.375 5.625C9.375 7.07475 8.19975 8.25 6.75 8.25C5.30025 8.25 4.125 7.07475 4.125 5.625C4.125 4.17525 5.30025 3 6.75 3C8.19975 3 9.375 4.17525 9.375 5.625Z"
-              stroke="currentColor"
-              strokeWidth="1.66667"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        }
-        text="Propose contribution"
-        onClick={() => {
-          selector
-            ?.wallet()
-            .then((wallet) => {
-              wallet
-                .signAndSendTransaction({
-                  actions: [
-                    {
-                      type: "FunctionCall",
-                      params: {
-                        gas: "300000000000000",
-                        deposit: "0",
-                        methodName: "add_request",
-                        args: {},
-                      },
-                    },
-                  ],
-                })
-                .then(console.log)
-                .catch(console.error);
-              // wallet
-              //   .signAndSendTransactions({
-              //     transactions: [
-              //       {
-              //         receiverId: "social.near",
-              //         actions: [
-              //           {
-              //             type: "FunctionCall",
-              //             params: {
-              //               gas: "300000000000000",
-              //               deposit: "0",
-              //               methodName: "set",
-              //               args: {
-              //                 [selector.store.getState().accounts.at(0)
-              //                   ?.accountId ?? ""]: {
-              //                   profile: {
-              //                     test: "",
-              //                   },
-              //                 },
-              //               },
-              //             },
-              //           },
-              //         ],
-              //       },
-              //     ],
-              //   })
-              //   .then(console.log)
-              //   .catch(console.error);
-            })
-            .catch(console.error);
-        }}
-      />
-      <CTA
-        color="green"
-        icon={
-          <svg
-            width="18"
-            height="18"
-            viewBox="0 0 18 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M12 5.25L14.25 7.5M14.25 7.5L16.5 5.25M14.25 7.5V3M12 15.75V14.85C12 13.5899 12 12.9598 11.7548 12.4785C11.539 12.0552 11.1948 11.711 10.7715 11.4952C10.2902 11.25 9.66012 11.25 8.4 11.25H5.1C3.83988 11.25 3.20982 11.25 2.72852 11.4952C2.30516 11.711 1.96095 12.0552 1.74524 12.4785C1.5 12.9598 1.5 13.5899 1.5 14.85V15.75M9.375 5.625C9.375 7.07475 8.19975 8.25 6.75 8.25C5.30025 8.25 4.125 7.07475 4.125 5.625C4.125 4.17525 5.30025 3 6.75 3C8.19975 3 9.375 4.17525 9.375 5.625Z"
-              stroke="currentColor"
-              strokeWidth="1.66667"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        }
-        text="Invite to contribute"
-      />
-    </>
-  ) : (
-    <></>
-  );
-
+export function CTAs() {
   return (
     <div className="flex flex-row flex-wrap items-center justify-start gap-3">
-      {signedInActions}
       <CTA
         color="gray"
         icon={
