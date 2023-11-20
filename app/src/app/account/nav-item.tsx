034b6a0a4b2406712cd074c94cbe4d0e4c869ca8
Petar Vujovic <petarvujovic98@gmail.com>
2023-11-20 19:27:16 +0100

    [WIP]: Improve navigation configuration

--- app/src/app/account/nav-item.tsx
+++ app/src/app/account/nav-item.tsx
@@ -12,8 +12,9 @@ import {
   AccordionTrigger,
 } from "~/components/ui/accordion";
 import { useProjectCompletion } from "~/hooks/projects";
-import { useParams, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useUser } from "~/stores/global";
+import { type NavItemProps } from "~/lib/constants/navigation/user";
 
 export function NavItem({
   section,
@@ -21,102 +22,124 @@ export function NavItem({
   label,
   subMenu,
   count,
-}: {
-  section: string;
-  icon: React.ReactNode;
-  label: React.ReactNode;
-  subMenu?: { section: string; label: string; progress: number }[];
-  count?: number;
-}) {
+}: NavItemProps) {
   const user = useUser();
   const { data: completion } = useProjectCompletion(user?.accountId ?? "");
-  const { section: activeSection, subSection: activeSubSection } = useParams()!;
-  const pathname = usePathname();
-  const [sectionPart, subSectionPart] = (pathname ?? "").split("/").slice(2);
+  const [sectionPart, subSectionPart] = usePathname().split("/").slice(2);
+  const isSectionActive = sectionPart === section;
 
-  const isSectionActive = activeSection === section || sectionPart === section;
-
-  if (subMenu && subMenu.length > 0) {
+  if (!subMenu || subMenu.length === 0) {
     return (
-      <NavigationMenuItem className="w-full">
-        <Accordion type="single" collapsible>
-          <AccordionItem value={section} className="m-0 border-none p-0">
-            <AccordionTrigger
-              className={cn(
-                "m-0 w-full p-0 text-sm font-semibold text-ui-elements-dark hover:no-underline",
-                {
-                  "text-text-link": isSectionActive,
-                }
-              )}
-            >
-              <div
-                className={cn(
-                  "flex flex-row items-center justify-start gap-3",
-                  isSectionActive
-                    ? "[&>svg]:text-text-link"
-                    : "[&>svg]:text-text-gray"
-                )}
-              >
-                {icon}
-                <span>{label}</span>
-              </div>
-            </AccordionTrigger>
-            <AccordionContent>
-              <div className="flex w-full flex-col items-start justify-start gap-3 pl-8 pt-2">
-                {completion &&
-                  subMenu.map(({ section: subSection, label }) => (
-                    <NavigationMenuLink
-                      key={subSection}
-                      href={`/account/${section}/${subSection}`}
-                      className={cn(
-                        "flex w-full flex-row items-center justify-between",
-                        {
-                          "text-text-link":
-                            isSectionActive &&
-                            (activeSubSection === subSection ||
-                              subSectionPart === subSection),
-                        }
-                      )}
-                    >
-                      <span>{label}</span>
-                      <small>
-                        {completion[
-                          subSection as keyof typeof completion
-                        ].toLocaleString("en-US", { style: "percent" })}
-                      </small>
-                    </NavigationMenuLink>
-                  ))}
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+      <NavigationMenuItem
+        className={cn("w-full text-sm font-semibold text-ui-elements-dark", {
+          "text-text-link": isSectionActive,
+        })}
+      >
+        <NavigationMenuLink
+          href={`/account/${section}`}
+          className="flex flex-row items-center justify-between"
+        >
+          <MenuItem
+            icon={icon}
+            label={label}
+            isSectionActive={isSectionActive}
+          />
+          {count}
+        </NavigationMenuLink>
       </NavigationMenuItem>
     );
   }
 
   return (
-    <NavigationMenuItem
-      className={cn("w-full text-sm font-semibold text-ui-elements-dark", {
-        "text-text-link": isSectionActive,
+    <NavigationMenuItem className="w-full">
+      <Accordion type="single" collapsible>
+        <AccordionItem value={section} className="m-0 border-none p-0">
+          <AccordionTrigger
+            className={cn(
+              "m-0 w-full p-0 text-sm font-semibold text-ui-elements-dark hover:no-underline",
+              { "text-text-link": isSectionActive }
+            )}
+          >
+            <MenuItem
+              icon={icon}
+              label={label}
+              isSectionActive={isSectionActive}
+              noGrow
+            />
+          </AccordionTrigger>
+          <AccordionContent>
+            <div className="flex w-full flex-col items-start justify-start gap-3 pl-8 pt-2">
+              {completion &&
+                subMenu.map(({ section: subSection, label }) => (
+                  <SubMenuItem
+                    key={subSection}
+                    section={section}
+                    subSection={subSection}
+                    completion={
+                      completion[subSection as keyof typeof completion]
+                    }
+                    active={isSectionActive}
+                    label={label}
+                    subSectionPart={subSectionPart}
+                  />
+                ))}
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </NavigationMenuItem>
+  );
+}
+
+function MenuItem({
+  icon,
+  label,
+  isSectionActive,
+  noGrow,
+}: {
+  icon: React.ReactNode;
+  label: React.ReactNode;
+  isSectionActive: boolean;
+  noGrow?: boolean;
+}) {
+  return (
+    <div
+      className={cn(
+        "flex w-full flex-row items-center justify-start gap-3",
+        isSectionActive ? "[&>svg]:text-text-link" : "[&>svg]:text-text-gray"
+      )}
+    >
+      {icon}
+      <span className={cn({ "flex-grow": !noGrow })}>{label}</span>
+    </div>
+  );
+}
+
+function SubMenuItem({
+  subSection,
+  label,
+  section,
+  active,
+  completion,
+  subSectionPart,
+}: {
+  subSection: string;
+  label: string;
+  section: string;
+  active: boolean;
+  completion: number;
+  subSectionPart?: string;
+}) {
+  return (
+    <NavigationMenuLink
+      key={subSection}
+      href={`/account/${section}/${subSection}`}
+      className={cn("flex w-full flex-row items-center justify-between", {
+        "text-text-link": active && subSectionPart === subSection,
       })}
     >
-      <NavigationMenuLink
-        href={`/account/${section}`}
-        className="flex flex-row items-center justify-between"
-      >
-        <div
-          className={cn(
-            "flex w-full flex-row items-center justify-start gap-3",
-            isSectionActive
-              ? "[&>svg]:text-text-link"
-              : "[&>svg]:text-text-gray"
-          )}
-        >
-          {icon}
-          <span className="flex-grow">{label}</span>
-        </div>
-        {count}
-      </NavigationMenuLink>
-    </NavigationMenuItem>
+      <span>{label}</span>
+      <small>{completion.toLocaleString("en-US", { style: "percent" })}</small>
+    </NavigationMenuLink>
   );
 }
