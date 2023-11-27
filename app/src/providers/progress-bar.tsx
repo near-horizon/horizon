35e59ab628c36683b191a3a2b6b375f42a044303
Petar Vujović <petarvujovic98@gmail.com>
2023-11-27 12:17:28 +0100

    chore: Update spinner

--- app/src/providers/progress-bar.tsx
+++ app/src/providers/progress-bar.tsx
@@ -5,13 +5,7 @@ import { AppProgressBar } from "next-nprogress-bar";
 export function ProgressBar({ children }: { children?: React.ReactNode }) {
   return (
     <>
-      <AppProgressBar
-        height="5px"
-        color="#66a0ff"
-        options={{ showSpinner: false }}
-        delay={200}
-        shallowRouting
-      />
+      <AppProgressBar height="5px" color="#66a0ff" shallowRouting />
       {children}
     </>
   );
