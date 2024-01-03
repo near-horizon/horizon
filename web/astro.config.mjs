b879e3e244c3ed26abd14091cbdd904adf5203e8
Petar Vujovic <petarvujovic98@gmail.com>
2024-01-03 13:50:53 +0100

    fix: Remove sentry

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,20 +3,20 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
-import sentry from "@sentry/astro";
+// import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
     tailwind(),
-    sentry({
-      dsn: "https://b177ae38c66bfa09555c55247cb49d02@o4506456115511296.ingest.sentry.io/4506456117149696",
-      sourceMapsUploadOptions: {
-        project: "landing-website",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-      },
-    }),
+    // sentry({
+    //   dsn: "https://b177ae38c66bfa09555c55247cb49d02@o4506456115511296.ingest.sentry.io/4506456117149696",
+    //   sourceMapsUploadOptions: {
+    //     project: "landing-website",
+    //     authToken: process.env.SENTRY_AUTH_TOKEN,
+    //   },
+    // }),
   ],
   output: "server",
   adapter: vercel({
